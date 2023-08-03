cd3a4153d6d6cd9e2884ce37315bee6f198aaff9
Alberto Aguilera <drimer.aaj@gmail.com>
2023-08-03 22:54:13 +0100

    First attempt at report email

--- src/email.py
+++ src/email.py
@@ -1,2 +1,19 @@
+import smtplib
+import os
+
+from email.mime.text import MIMEText
+
+
 def send_email(body: str):
-    pass
+    recipient_email = os.environ['RECIPIENT_EMAIL_ADDRESS']
+    from_email_address = os.environ['FROM_EMAIL_ADDRESS']
+    from_email_password = os.environ['FROM_EMAIL_PASSWORD']
+
+    msg = MIMEText(body)
+    msg['Subject'] = 'Daily report'
+    msg['From'] = from_email_address
+    msg['To'] = recipient_email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+        smtp_server.login(from_email_address, from_email_password)
+        smtp_server.sendmail(from_email_address, recipient_email, msg.as_string())
+    print("Message sent!")
