06a693831dd8b595753562b7fa2244f68550a292
Alberto Aguilera <drimer.aaj@gmail.com>
2023-08-12 09:42:23 +0100

    Fix pipeline

--- src/notion/api_client.py
+++ src/notion/api_client.py
@@ -3,13 +3,6 @@ import os
 
 from src.notion.models import Task
 
-token = os.environ['NOTION_API_TOKEN']
-DEFAULT_HEADERS = {
-    "Authorization": "Bearer " + token,
-    "Content-Type": "application/json",
-    "Notion-Version": "2022-06-28"
-}
-
 
 EXAMPLE_FILTER = {
   "filter": {
@@ -37,15 +30,17 @@ EXAMPLE_FILTER = {
 }
 
 
-def pull_db_entries_from_notion(database_id: str, filters: dict):
-    '''
+def get_request_headers():
+    return {
+        "Authorization": "Bearer " + os.environ.get('NOTION_API_TOKEN', None),
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28"
+    }
 
-    :param database_id:
-    :param filters:
-    :return:
-    '''
+
+def pull_db_entries_from_notion(database_id: str, filters: dict):
     read_url = f"https://api.notion.com/v1/databases/{database_id}/query"
-    res = requests.request("POST", read_url, headers=DEFAULT_HEADERS, json=filters)
+    res = requests.request("POST", read_url, headers=get_request_headers(), json=filters)
     data = res.json()
     print(res.status_code)
 
@@ -57,9 +52,6 @@ def as_task(api_json_response: dict) -> Task:
         due_by = api_json_response['properties']['Due']['date']['end']
     else:
         due_by = None
-
-    if not api_json_response['properties']['Task']['title']:
-        a = 3
 
     return Task(
         title=api_json_response['properties']['Task']['title'][0]['text']['content'],
