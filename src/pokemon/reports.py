c80db68905c5fb1f4e3e2de59c6533a4bff4f39c
Alberto Aguilera <drimer.aaj@gmail.com>
2023-09-19 17:24:58 +0100

    Get Pokemon news from newapi.org

--- src/pokemon/reports.py
+++ src/pokemon/reports.py
@@ -1,5 +1,12 @@
 from src.pokemon.models import PokemonNewsReport
+from src.pokemon.news_api_client import get_latest_pokemon_news, as_pokemon_news_items
 
 
 def build_daily_report():
-    return PokemonNewsReport(news_items=[])
+    articles_response = get_latest_pokemon_news()
+    news_items = as_pokemon_news_items(articles_response)
+    return PokemonNewsReport(news_items=news_items)
+
+
+if __name__ == '__main__':
+    build_daily_report()
