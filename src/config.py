286d11c32526de67c979eb7cde3798079ecfa345
Alberto Aguilera Juarez <alberto.aguilera@xdesign.com>
2024-02-09 17:04:58 +0000

    Add unit test for config

--- src/config.py
+++ src/config.py
@@ -13,16 +13,3 @@ class Config:
             raise ConfigError('Could not get the value for env variable {}'.format(param_name))
 
         return value
-
-
-# def test_config():
-#     class TestConfig(Config):
-#         TEST_PARAM: str = Config.get_from_env('TEST_PARAM')
-#
-#     env_value = 'test_value'
-#     os.environ.get = Mock(return_value=env_value)
-#
-#     config = TestConfig()
-#
-#     os.environ.get.assert_called_once_with('TEST_PARAM')
-#     assert config.TEST_PARAM == env_value
--- src/test_placeholder.py
+++
@@ -1,8 +0,0 @@
-"""
-This file is here temporarily just to make pytest happy.
-To delete after the first working unit test.
-"""
-
-
-def test_empty():
-    pass
