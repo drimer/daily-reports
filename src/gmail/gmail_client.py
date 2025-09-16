ccecd8e27dee9bf901158c93bd5c90426096aac8
Alberto Aguilera <drimer.aaj@gmail.com>
2025-09-16 09:07:57 +0100

    feat: identify bin tag name accounting for different language settings

--- src/gmail/gmail_client.py
+++ src/gmail/gmail_client.py
@@ -2,11 +2,30 @@ import datetime
 import imaplib
 
 
+def infer_bin_directory_name(mail: imaplib.IMAP4_SSL) -> str:
+    _, *directory_tags = mail.list()[1]
+
+    known_bin_names = ("[Gmail]/Trash", "[Gmail]/Bin")
+
+    for tag in directory_tags:
+        for known_bin_name in known_bin_names:
+            if known_bin_name in tag.decode():
+                return known_bin_name
+
+    return None
+
+
 def delete_old_emails(older_than_days, email, password):
     print(f"Deleting emails older than {older_than_days} days...")
 
     mail = imaplib.IMAP4_SSL("imap.gmail.com")
     mail.login(email, password)
+
+    bin_directory = infer_bin_directory_name(mail)
+    if bin_directory:
+        print(f"Inferred bin directory: {bin_directory}")
+    else:
+        raise Exception("Could not infer bin directory name.")
 
     mail.select('"[Gmail]/All Mail"')
     before_date = datetime.datetime.now() - datetime.timedelta(
@@ -28,7 +47,7 @@ def delete_old_emails(older_than_days, email, password):
             )
 
             print(f"Deleting email with date: {date_time} subject: {subject}")
-            mail.uid("move", uid, "[Gmail]/Trash")
+            mail.uid("move", uid, bin_directory)
 
     mail.close()
     mail.logout()
